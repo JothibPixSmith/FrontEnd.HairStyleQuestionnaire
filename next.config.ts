2576254e76308cecca4a680b58b22f59f3d337d3
Jonathan Thibault <Jothib@outlook.com>
2026-04-23 02:44:10 -0400

    missed this one

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,7 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath,
   assetPrefix: basePath,
-  trailingSlash: true,
-00ed14ee0f17c605be3516200054f0f6f6c052c
+  trailingSlash: true
 };
 
 export default nextConfig;
